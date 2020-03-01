410464929af6e3d1ff891184c27837beb87ea5c9
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-03-02 01:05:30 +0200

    still working within

---
+++ src/component/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ToDo from './todo.js';
+// import Header from './header.js';
+// import Footer from './footer.js';
+
+function App (){
+  return (
+    <React.Fragment>
+      {/* <Header /> */}
+      <ToDo />
+      {/* <Footer /> */}
+    </React.Fragment>
+  );
+}
+export default App;
